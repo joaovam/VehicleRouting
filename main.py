5fe9bb705ee506877256ba75758d0587e7499c95
joaovam <joao.vieira.1275044@sga.pucminas.br>
2023-10-02 15:13:47 -0300

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import igraph as graph
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    edge = []
+    for i in range(0, 10):
+        for j in range(i, 10):
+            edge.append((i, j))
+
+    g = graph.Graph(10, edge)
+    g.es['peso'] = [12, 11, 7, 10, 10, 9, 8, 6, 12,
+                    8, 5, 9, 12, 14, 16, 17, 22,
+                    9, 15, 17, 8, 18, 14, 22,
+                    7, 9, 11, 12, 12, 17,
+                    3, 17, 7, 15, 18,
+                    18, 6, 15, 15,
+                    16, 8, 16,
+                    11, 11,
+                    10]
+    demands = [10, 15, 18, 17, 3, 5, 9, 4, 6]
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()

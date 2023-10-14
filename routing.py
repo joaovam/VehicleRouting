8086699f511c7c9f98305060bd3e6c7daf01cd6c
Joao Victor Amorim Vieira <joao.vieira.1275044@sga.pucminas.br>
2023-10-14 09:24:44 -0300

    Update routing.py

--- routing.py
+++ routing.py
@@ -14,18 +14,6 @@ class Rota:
 def solveRouting(g: graph.Graph, demands):
     basic_costs = {}  # edges going from node 0 to the others
     gain = []
-    # (Rota, ganho)
-
-    # 1. montar tabela de ganho
-    #     1.1 ordenar tabela tendo como base o ganho ** só usar função sort
-    #
-    # 2. fusão das rotas, limitando pela demanda
-    #     2.1 testar se fusão é valida
-    #     2.2 efetuar a fusão, criar uma nova rota, atualizar a rota antiga e apaga as rotas utilizadas
-    #
-    #     "1-2-3" |
-    #     "5-6" |
-    #
     for e in g.es:
         if e.source == 0:
             basic_costs[e.target] = e["peso"]
